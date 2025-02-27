c2c7588de0cdf18ab235a85fdd6a9c2429c7265a
guillaume-bomben <guillaume.bomben@laplateforme.io>
2025-02-27 11:07:49 +0100

    finish graph

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 
 int main() {
     Graph g;
-    g.addVertex(1);
-    g.addVertex(2);
-    g.addVertex(3);
-    g.addVertex(4);
-    g.addVertex(5);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 4);
-    g.addEdge(3, 5);
+    g.addVertex("1");
+    g.addVertex("2");
+    g.addVertex("3");
+    g.addVertex("4");
+    g.addVertex("5");
+    g.addEdge("1", "2");
+    g.addEdge("1", "3");
+    g.addEdge("2", "4");
+    g.addEdge("3", "5");
+    g.removeVertex("3");
     g.printGraph();
     return 0;
 }
--- src/graph.cpp
+++ src/graph.cpp
@@ -3,15 +3,24 @@
 Graph::Graph(){}
 Graph::~Graph(){}
 
-void Graph::addVertex(int v) {
+void Graph::addVertex(std::string v) {
     adjList[v] = {};
 }
 
-// Graph_Class::removeVertex(int v) {
-//     adjList.erase(v);
-// }
+void Graph::removeVertex(std::string v) {
+    if (adjList.find(v) == adjList.end()) {
+        return;
+    }
+    else {
+        adjList.erase(v);
+    }
 
-void Graph::addEdge(int v1, int v2) {
+    for (auto const& [vertex, adj] : adjList) {
+        adjList[vertex].erase(std::remove(adjList[vertex].begin(), adjList[vertex].end(), v), adjList[vertex].end());
+    }
+}
+
+void Graph::addEdge(std::string v1, std::string v2) {
     adjList[v1].push_back(v2);
     adjList[v2].push_back(v1);
 }
@@ -26,3 +35,16 @@ void Graph::printGraph() {
     }
 }
 
+
+void Graph::removeEdge(std::string v1, std::string v2) {
+    adjList[v1].erase(std::remove(adjList[v1].begin(), adjList[v1].end(), v2), adjList[v1].end());
+    adjList[v2].erase(std::remove(adjList[v2].begin(), adjList[v2].end(), v1), adjList[v2].end());
+}
+
+std::string Graph::getVertex(std::string i) {
+    return adjList[i].front();
+}
+
+std::string Graph::getEdge(std::string i, std::string j) {
+    return adjList[i].front();
+}
